d43806341469403a156210aa69b1af3e5d325142
Rustuq <rustuq936@gmail.com>
2023-03-31 17:45:40 +0700

    Push Tugas03

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rustuq Mohammad Fahlefi
+              Saya tinggal di daerah Pondok Aren
+              olah raga yang saya sukai adalah
+              Bulutangkis";
+    }
+}
